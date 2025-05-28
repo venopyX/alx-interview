d6e2ed79d5028c9418c6f7c66027e1822f45661e
Gemechis Chala <gladsonchala@gmail.com>
2025-05-28 23:58:15 +0300

    feat: Stars wars api soln

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,53 +3,46 @@
 const request = require('request');
 
 /**
- * Fetches and prints the names of characters in a Star Wars movie.
+ * Fetches and prints the names of characters in a Star Wars movie in the same order as the characters list.
  * @param {string} movieId - The ID of the Star Wars movie.
  */
-async function fetchMovieCharacters(movieId) {
+function fetchMovieCharacters(movieId) {
   const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-  try {
-    /**
-     * Fetches the list of character URLs for a given movie ID.
-     * @returns {Promise<Array>>} A promise that resolves to an array of character URLs.
-     */
-    const characters = await new Promise((resolve, reject) => {
-      request(url, (err, res, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(JSON.parse(body).characters);
-        }
-      });
-    });
-
-    /**
-     * Iterates over character URLs, fetches each character's name, and prints it.
-     */
-    for (const characterUrl of characters) {
-      const name = await new Promise((resolve, reject) => {
-        request(characterUrl, (err, res, body) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(body).name);
-          }
-        });
-      });
-      console.log(name);
+  request(url, function (err, res, body) {
+    if (err) {
+      console.error('Error:', err);
+      return;
     }
-  } catch (err) {
-    console.error('Error:', err.message);
-  }
+
+    const actors = JSON.parse(body).characters;
+    printCharactersInOrder(actors, 0);
+  });
 }
 
 /**
- * Main execution: Checks if a movie ID is provided and fetches the movie characters.
+ * Recursively prints character names in the order of the provided URLs.
+ * @param {Array} actors - Array of character URLs.
+ * @param {number} index - Current index in the actors array.
  */
+function printCharactersInOrder(actors, index) {
+  if (index === actors.length) return;
+
+  request(actors[index], function (err, res, body) {
+    if (err) {
+      console.error('Error:', err);
+      return;
+    }
+
+    console.log(JSON.parse(body).name);
+    printCharactersInOrder(actors, index + 1);
+  });
+}
+
+// Check if a movie ID is provided
 const movieId = process.argv[2];
 if (!movieId) {
-  console.error('Usage: ./script.js <Movie ID>');
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
   process.exit(1);
 }
 
